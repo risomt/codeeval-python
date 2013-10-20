81ce14e9244ea9ccb443241d1c9edeed0d0884ff
risomt <risomt@gmail.com>
2013-10-19 20:26:59 -0400

    Slighty cleaner, same hack

--- 27.py
+++ 27.py
@@ -22,6 +22,7 @@ Output sample:
 """
 
 # if we're allowed to use functions: print(bin(int(line.strip()))[2:])
+# algorithm derived from http://www.wikihow.com/Convert-from-Decimal-to-Binary #2
 
 from sys import argv		
 
@@ -29,19 +30,22 @@ with open(argv[1]) as data:
 	for line in data.readlines():
 	
 		# parse out the number to generate in binary
-		original= int(line.strip())
+		original = int(line.strip())
 		number = original
 		binary = []
 
 		while True:
+			# if we hit 0 we're done!
 			if number == 0:
-				# hack
+				# hack - the (last) 2^0 is not coming out correctly
+				# this makes the 2^0 position 1 if the original is odd
 				binary[-1] = original % 2
 				break
 			else:
-				remainder = number / 2
+				# divide by base (2) to get new number and continue on
 				number = number / 2
 
-				binary.append(remainder % 2)
+				# determine whether to add a 1 or 0
+				binary.append(number % 2)
 
 		print "".join(map(str, binary))
