6ce8e28529640b30bee99c46eb61e2f0935162bb
risomt <risomt@gmail.com>
2013-11-03 23:36:35 -0500

    Cleaned up comments slightly.

--- 14.py
+++ 14.py
@@ -23,17 +23,25 @@ Output sample:
 
 from sys import argv
 
+# suggestion from http://stackoverflow.com/questions/104420/how-to-generate-all-permutations-of-a-list-in-python/104436#104436
+# much nicer than my versions!
 def permutations(elements):
         if len(elements) <=1:
                 yield elements
         else:
-                for permute in permutations(elements[1:]):
+                # for each set of characters in [hat] - [at] - [t] (recursive call)
+                for permutation in permutations(elements[1:]):
+                        # create all possible combinations with given set and return
                         for i in range(len(elements)):
-                                yield permute[:i] + elements[0:1] + permute[i:]
+                                # yield (return generator) each possible permutation to parent
+                                yield permutation[:i] + elements[0:1] + permutation[i:]
 
 
 with open(argv[1]) as data:
         for line in data.readlines():
 
-               characters = [x for x in line.strip()]
-               print ",".join(str("".join(x)) for x in permutations(characters))
+                # print out a list of all unique permutations for each string in the file 
+                # permutations() returns a list of lists of the permutations, hence the
+                # double join to convert each item in the list, then the list itself into CSV
+                # test expects answer to be alphabetically sorted.. and one linered it
+                print ",".join(sorted([str("".join(x)) for x in permutations([x for x in line.strip()])]))
