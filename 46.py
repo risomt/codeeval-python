2d2fe04e83d16d1088ba534a474cb754bd6ee638
risomt <risomt@gmail.com>
2013-11-18 19:41:25 -0500

    PEP8 formatting

--- 46.py
+++ 46.py
@@ -1,11 +1,15 @@
 """
 Challenge Description:
 
-        Print out the prime numbers less than a given number N. For bonus points your solution should run in N*(log(N)) time or better. You may assume that N is always a positive integer.
+        Print out the prime numbers less than a given number N. For bonus
+        points your solution should run in N*(log(N)) time or better. You may
+        assume that N is always a positive integer.
 
 Input sample:
 
-        Your program should accept as its first argument a path to a filename. Each line in this file is one test case. Each test case will contain an integer n < 4,294,967,295. E.g.
+        Your program should accept as its first argument a path to a filename.
+        Each line in this file is one test case. Each test case will contain an
+        integer n < 4,294,967,295. E.g.
 
         10
         20
@@ -13,12 +17,15 @@ Input sample:
 
 Output sample:
 
-        For each line of input, print out the prime numbers less than N, in ascending order, comma delimited. (There should not be any spaces between the comma and numbers) E.g.
+        For each line of input, print out the prime numbers less than N, in
+        ascending order, comma delimited. (There should not be any spaces
+        between the comma and numbers) E.g.
 
         2,3,5,7
         2,3,5,7,11,13,17,19
         2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97
 """
+
 
 # taken from one of the earlier tests
 def isprime(number):
@@ -32,8 +39,9 @@ with open(argv[1]) as data:
         for line in data.readlines():
                 number = int(line.strip())
 
-                # loop through all values from 2 to number 
-                # why 2? 0 and 1 are not prime numbers (also - see output examples)
+                # loop through all values from 2 to number
+                # why 2? 0 and 1 are not prime numbers
+                # also - see output exampes
                 primes = []
                 for x in range(2, number):
                         if isprime(x):
