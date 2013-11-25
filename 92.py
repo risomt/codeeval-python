c36b1e3c33cb18490ac411c8b97f841a61208a8c
risomt <risomt@gmail.com>
2013-11-25 17:28:01 -0500

    Adding 92.

---
+++ 92.py
@@ -0,0 +1,30 @@
+"""
+Challenge Description:
+
+        Write a program which finds the next-to-last word in a string.
+
+Input sample:
+
+        Your program should accept as its first argument a path to a
+        filename. Input example is the following
+
+        some line with text
+        another line
+
+        Each line has more than one word.
+
+Output sample:
+
+        Print the next-to-last word in the following way.
+
+        with
+        another
+"""
+
+from sys import argv
+
+with open(argv[1]) as data:
+        for line in data.readlines():
+
+                lines = line.strip().split()
+                print lines[-2]
